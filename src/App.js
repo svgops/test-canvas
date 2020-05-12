6beed250c41bd4003c970664c8402739be83ac45
svgops <64754167+svgops@users.noreply.github.com>
2020-05-12 10:54:20 -0600

    update
    add image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./styles.css";
+import Canvas from "./Canvas";
+import Example from "./ergita-sela-rose.jpg";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <img ref="image" src={Example} alt="MyImage" />
+      <Canvas />
+      <div className="Test" />
     </div>
   );
 }
--- src/Canvas.js
+++ src/Canvas.js
@@ -1,10 +1,25 @@
 import React from "react";
 
 export default function Canvas() {
+  const img = new Image();
+  img.src = "./ergita-sela-rose.jpg";
+
+  const canvasRef = React.useRef(null);
+
   return (
-    <div>
-      <canvas ref="canvas" width={640} height={425} />
-      <img ref="image" src={cheese} className="hidden" />
-    </div>
+    <canvas
+      ref={canvasRef}
+      width={window.innerWidth}
+      height={window.innerHeight}
+      onClick={e => {
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext("2d");
+        // implement draw on ctx here
+        ctx.drawImage(img, 0, 0);
+        img.style.display = "none";
+        const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        const data = imageData.data;
+      }}
+    />
   );
 }
