e70033e1fb0518d2faa32b5d1d88c82f4a4567f8
svgops <64754167+svgops@users.noreply.github.com>
2020-05-12 14:25:42 -0600

    image-loading-lodash
    Image loaded

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
-import React from "react";
-import "./styles.css";
+import React, { useState } from "react";
 import Canvas from "./Canvas";
+import cx from "classnames";
 import Image from "./Image";
+import "./styles.css";
+
+const srcs = [
+  "https://images.unsplash.com/photo-1551923922-a0d2cccc164b?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+  "https://images.unsplash.com/photo-1561238160-3fd50893667f?ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+  "https://images.unsplash.com/photo-1571286264720-76bd4ee3ea85?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"
+];
+
+const useSrcs = () => {
+  const [selectedSrc, setSrc] = useState(srcs[0]);
+  return {
+    buttons: srcs.map((src, i) => (
+      <button
+        className={cx({ active: src === selectedSrc })}
+        onClick={() => setSrc(src)}
+        key={src}
+      >
+        image {i + 1}
+      </button>
+    )),
+    // force to re-render
+    // this is just example
+    image: srcs.map(src =>
+      src === selectedSrc ? <Image key={src} src={selectedSrc} /> : null
+    )
+  };
+};
 
 export default function App() {
+  const { buttons, image } = useSrcs();
+
   return (
     <div className="App">
-      <Image url="https://unsplash.com/photos/rvCCi0GUpFI" />
+      {image}
+      <br />
+      {buttons}
     </div>
   );
 }
--- src/Image.js
+++ src/Image.js
@@ -1,6 +1,10 @@
 import React from "react";
-import Img from "react-image";
+import cx from "classnames";
+import useImage from "./useImage";
 
-const Image = ({ url }) => <Img src={url} />;
+const Image = ({ src, alt = "" }) => {
+  const { loaded } = useImage({ src });
+  return <img className={cx("smooth", { loaded })} src={src} alt={alt} />;
+};
 
 export default Image;
